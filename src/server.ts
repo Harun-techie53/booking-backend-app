e37e7e1d3f38e5cd7257d515072e77ca6eb88707
Harun-techie53 <u1809030@student.cuet.ac.bd>
2024-08-20 17:22:41 -0400

    removed cors restriction

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 dotenv.config();
 import dbConnect from "../config/db.config";
@@ -14,6 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 dbConnect();
 
