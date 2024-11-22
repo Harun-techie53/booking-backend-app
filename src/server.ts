ecf29570dc93c5f1c781bce8a20fe2fb15cd2636
Harun-techie53 <harunurrashid861@gmail.com>
2024-11-22 08:34:51 -0500

    allow some access control logic

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: false,
+  })
+);
 
 app.use(function (request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");
